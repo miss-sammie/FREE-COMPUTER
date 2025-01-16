a2d93e415023d5f3fd1c338f37d708c5ee728005
bloomingbridges <brueckner.florian@googlemail.com>
2025-01-16 03:15:54 +0100

    Added Library class.
    Moved getfilenames.py logic into Library class.

--- server/server.py
+++ server/server.py
@@ -1,66 +1,47 @@
-# from http.server import HTTPServer, BaseHTTPRequestHandler
-from bottle import route, run, static_file, redirect
-import mimetypes
+from bottle import route, run, static_file, redirect, response
+from library import Library
+from os import getcwd
 import webbrowser
-import os
+import mimetypes
 
 ###########################################################################
 # CONFIGURATION ###########################################################
 ###########################################################################
 PORT = 9000
 AUTO_LAUNCH_BROWSER = False
+LIBRARY = Library()
 ###########################################################################
 
 ###########################################################################
-# ROUTES ////////////######################################################
+# ROUTES ##################################################################
 ###########################################################################
 @route('/')
 def home():
     redirect("/index.html")
 
+@route('/library')
+def serve_library_json():
+    response.content_type = "application/json; charset=UTF8"
+    return LIBRARY.as_json()
+
 @route('/<filepath:path>')
 def serve_library_asset(filepath):
-    return static_file(filepath, root=os.getcwd())
-
+    return static_file(filepath, root=getcwd())
 ###########################################################################
-# CARRIER_BAG_SERVER ######################################################
-###########################################################################
-# class CarrierBagServer(BaseHTTPRequestHandler):
-#     def do_GET(self):
-#         if self.path == "/":
-#             self.path = "/index.html"
-#         # print("Trying to find " + self.path)
-#         mime_type, encoding = mimetypes.guess_type(self.path)
-#         try:
-#             requested_file = open(self.path[1:]).read()
-#             self.send_response(200)
-#         except:
-#             requested_file = "No such file on here I'm afraid :/"
-#             self.send_response(404)
-#         self.send_header("Content-type", mime_type)
-#         self.end_headers()
-#         self.wfile.write(bytes(requested_file, "utf-8"))
-
-#     def get_library(self, url):
-#         print("Requested Library file: " + url)
 
 ###########################################################################
 # UTILITY FUNCTIONS #######################################################
 ###########################################################################
 def launch_browser():
     webbrowser.open_new("http://localhost:" + str(PORT))
+###########################################################################
 
 ###########################################################################
 # MAIN ####################################################################
 ###########################################################################
 if __name__ == "__main__":
+    LIBRARY.index()
     if AUTO_LAUNCH_BROWSER:
         launch_browser()
     run(host='0.0.0.0', port=PORT, debug=True)
-    # server = HTTPServer(("0.0.0.0", PORT), CarrierBagServer)
-    # try:
-    #     server.serve_forever()
-    # except KeyboardInterrupt:
-    #     pass
-    # server.server_close()
-    # print("👋 Bye for now")
+###########################################################################
